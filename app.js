1302be4ddb80ab758262fb6c3f0ef84340712316
Felix Odebe <fodebe@mds.com>
2023-11-22 09:37:36 +0100

    fix cors issue

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
 app.use(express.json());
 // extra packages
 app.use(helmet());
-app.use(cors);
+app.use(cors());
 app.use(xss());
 
 const authenticationMiddleware = require('./middleware/authentication');
@@ -47,7 +47,7 @@ app.use('/api/v1/jobs', authenticationMiddleware, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
