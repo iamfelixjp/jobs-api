2ed04422c58dc6dacb85d5aa288b4e1c2c6bf009
Felix Odebe <fodebe@mds.com>
2023-11-22 11:39:05 +0100

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -37,8 +43,9 @@ app.use(xss());
 const authenticationMiddleware = require('./middleware/authentication');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Jobs API</h1><a>Documentation</a>');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // use routes
 app.use('/api/v1/auth', authRouter);
